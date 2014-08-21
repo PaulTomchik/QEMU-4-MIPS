f7bfb510dcc4bc7ea613f535ca991cccc94fc1ff
Paul Tomchik <paultomchik@gmail.com>
2014-08-21 09:22:49 -0400

    The host setup.

---
+++ README.txt
@@ -0,0 +1,2 @@
+Debian for MIPS Qemu images:
+	https://people.debian.org/~aurel32/qemu/mips/
---
+++ runDebianMIPS.sh
@@ -0,0 +1,1 @@
+qemu-system-mips64 -m 1048 -M malta -kernel vmlinux-3.2.0-4-5kc-malta -hda wheezy_mips.qcow2 -nographic -append "root=/dev/sda1 console=ttyS0" 
